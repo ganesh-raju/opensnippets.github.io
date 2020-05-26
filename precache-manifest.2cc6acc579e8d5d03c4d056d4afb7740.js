1a0a07b2810fcd97aee8fea2171fc9786ff75314
Ganesh <ganeshmuppallasv@gmail.com>
2020-05-26 11:37:35 +0530

    Updates

---
+++ precache-manifest.2cc6acc579e8d5d03c4d056d4afb7740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5620d4a20660e513a76b4c993fa2f0",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "486009b6cdc31d5782fc",
+    "url": "/my-app/static/css/main.e9f124ae.chunk.css"
+  },
+  {
+    "revision": "227dd319c4c27034f78b",
+    "url": "/my-app/static/js/2.56288373.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.56288373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486009b6cdc31d5782fc",
+    "url": "/my-app/static/js/main.d2dae893.chunk.js"
+  },
+  {
+    "revision": "ec087977d06fd7bf206e",
+    "url": "/my-app/static/js/runtime-main.4bfb0f9f.js"
+  }
+]);
